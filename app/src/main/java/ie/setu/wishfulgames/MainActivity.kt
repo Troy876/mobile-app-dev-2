5a33da6b48e9c2f0a8799246a57a5f5e8f758fd9
Troy876 <barrtroy02@gmail.com>
2025-02-22 10:26:28 +0000

    Add simple add screen.

--- app/src/main/java/ie/setu/wishfulgames/MainActivity.kt
+++ app/src/main/java/ie/setu/wishfulgames/MainActivity.kt
@@ -5,27 +5,74 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import ie.setu.wishfulgames.ui.theme.WishfulgamesJPCTheme
 import timber.log.Timber
 import timber.log.Timber.i
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         Timber.plant(Timber.DebugTree())
-        i("Placemark MainActivity started..")
+        i("Wishfulgames MainActivity started..")
         setContent {
             WishfulgamesJPCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = stringResource(id = R.string.app_name),
+                                    color = Color.White
+                                )
+                            },
+                            colors = TopAppBarDefaults.largeTopAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary
+                            ),
+                            navigationIcon = {
+                                Icon(imageVector = Icons.Filled.Menu,
+                                    contentDescription = "Menu",
+                                    tint = Color.White)
+                            }
+                        )
+                    }
+                )
+                { innerPadding ->
+                    AddGame(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,17 +82,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun AddGame(modifier: Modifier = Modifier) {
+    var title by remember { mutableStateOf("") }
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(all = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.primary,
+                cursorColor = MaterialTheme.colorScheme.primary
+            ),
+            value = title,
+            onValueChange = { title = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text(stringResource(id = R.string.text_gameTitleHint)) },
+            trailingIcon = {
+                Icon(
+                    Icons.Default.Edit, contentDescription = "",
+                    tint = Color.Black
+                )
+            },
+        )
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            elevation = ButtonDefaults.buttonElevation(20.dp)
+        ) {
+            Icon(Icons.Default.Add, contentDescription = "Add")
+            Spacer(modifier = Modifier.width(width = 4.dp))
+            Text(stringResource(id = R.string.button_addGame))
+        }
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AddGamePreview() {
     WishfulgamesJPCTheme {
-        Greeting("Android")
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.app_name),
+                            color = Color.White
+                        )
+                    },
+                    colors = TopAppBarDefaults.largeTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    ),
+                    navigationIcon = {
+                        Icon(imageVector = Icons.Filled.Menu,
+                            contentDescription = "Menu",
+                            tint = Color.White)
+                    }
+                )
+            },
+
+            )
+        { innerPadding ->
+            AddGame(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
