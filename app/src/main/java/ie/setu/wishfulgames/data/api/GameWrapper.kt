17f156429b8e080316ed51024993b9512f36d9d4
Troy876 <barrtroy02@gmail.com>
2025-04-07 22:24:12 +0100

    Remove deprecated persistence modules.

--- app/src/main/java/ie/setu/wishfulgames/data/api/GameWrapper.kt
+++
@@ -1,8 +0,0 @@
-package ie.setu.wishfulgames.data.api
-
-import ie.setu.wishfulgames.data.GameModel
-
-class GameWrapper {
-    var title: String? = null
-    var data: GameModel? = null
-}
--- app/src/main/java/ie/setu/wishfulgames/data/api/ServiceEndPoints.kt
+++
@@ -1,8 +0,0 @@
-package ie.setu.wishfulgames.data.api
-
-class ServiceEndPoints {
-    companion object {
-        const val BASE_URL= "https://wishfulgamesweb-single-server.onrender.com/"
-        const val GAMES_ENDPOINT = "games"
-    }
-}
